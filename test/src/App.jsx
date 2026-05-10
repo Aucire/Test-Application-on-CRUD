2e7934e68b3d9e8b0bfad509861b3cbaea72faf3
Aucire <jamesandaro0@gmail.com>
2026-05-10 16:48:42 +0300

    minor changes

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,51 +1,66 @@
 import { useEffect, useState } from 'react'
+import "./index.css"
 
 
-const baseUrl = "https://jsonplaceholder.typicode.com/todos"
+const baseUrl = import.meta.env.VITE_API_BASE_URL
 
-function App() {
+const App=()=>{
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [submittedTask, setsubmittedTask] = useState({})
+    const [todos, setTodos] = useState([])
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleSubmit=event=>{
+        event.preventDefault()
 
-        const newTask = { title, description }
+        if(!title.trim()||!description.trim()){
+            alert("Both fields are needed")
+            return
+        }
 
-            fetch(baseUrl, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(newTask)
-            })
-            .then((response) => {
-                if (!response.ok) { throw new Error("Failed") }
-                return response.json()
-            })
-            .then((data) => {
-                setsubmittedTask(data)
-                alert("Task submitted successfully")
+        const newTask = {title,description}
+            
+        fetch(baseUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newTask)
+        })
+        .then((response) => {
+            if (!response.ok) { throw new Error("Failed") }
+            return response.json()
+        })
+        .then((data) => {
+            setsubmittedTask(data)
+            alert("Task submitted successfully")
 
+            setTimeout(()=>{
                 setDescription("")
-                setTitle("")
-            })
-            .catch((error) => console.log(error))
+                setTitle("")                    
+            },2000)
 
+        })
+        .catch((error) => console.log(error))
+        .finally()
     }
 
+    useEffect(() =>{
 
-    //THE USEEFFECT IS NOT WORKING......!!!
-    //WITHOUT THE USEEFFECT IT RE-RENDERS WITHE EACH KEY-STROKE
-    useEffect(()=>{ 
-        console.log("IT ENTERED THE USEEFFECT;")
         fetch(baseUrl)
-            .then(res=>res.json())
-            .then(data=>console.log("task added;",data))
-            .catch(error=>console.error(error))  
-     },[])
+            .then(res => {
+                if (!res.ok) { throw new Error("Failed") }
+                return res.json()
+            })
+            .then(data => {
+                setTodos(data)
+                console.log("Data from API >> ", data)
+            })
+            .catch(error => console.error(error))
+    }, []);
+
+    const tasks = [submittedTask, ...todos] 
 
     return (
-        <main className='text-center bg-zinc-800 h-200 w-full p-20'>
+        <main className='text-center bg-zinc-800 min-h-200 w-full p-20'>
             <h1 className='text-red-600 text-4xl font-extrabold mb-5'>THIS IS A TASK MANAGEMENT APPLICATION</h1>
             <div className='text-xl'>
                 <form onSubmit={handleSubmit}>
@@ -53,28 +68,32 @@ function App() {
                     <input
                         className='m-2 border border-white border-4 rounded-md p-2 text-gray-300 text-2xl outline-0'
                         type='text'
-                        id='Title'
                         placeholder='Title of the task'
                         value={title}
-                        onChange={(e) => setTitle(e.target.value)} /><br />
+                        onChange={(e) => setTitle(e.target.value)} 
+                    /><br />
                     <label className='m-2 text-white font-bold text-xl'>Description of the Task;</label><br />
                     <input
                         className='m-2 border border-white border-4 rounded-md p-2 text-gray-300 text-2xl outline-0'
                         type='text'
-                        id='description'
                         placeholder='Description of the task'
                         value={description}
-                        onChange={(e) => setDescription(e.target.value)} /><br />
-                    <button 
+                        onChange={(e) => setDescription(e.target.value)}
+                    /><br />
+                    <button
                         className='m-4 bg-transparent text-red-600 border border-red-600 p-2 rounded-md'
-                        type='submit'>Submit</button>
+                        type='submit'>
+                            Submit
+                    </button>
                 </form>
                 <h2>The Submitted Task</h2>
                 <div className='text-blue-700 font-bold text-2xl'>{submittedTask.title}:{submittedTask.description}</div>
-                    <>Data from API</>
-                <div>
-
-                </div>
+                
+                <ol className='text-gray-200 font-bold'>
+                    {tasks.map((task, index) => (
+                        <li key={index} className='text-gray-300'>{task.title}</li>
+                    ))}
+                </ol>
             </div>
         </main>
     )
